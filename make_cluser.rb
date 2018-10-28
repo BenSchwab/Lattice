9d7039655e6fb13d73c7ad4f0f400966a1ff4a46
Ben Schwab <ben.schwab@airbnb.com>
2018-10-28 15:17:50 -0700

    Some cluster script

--- make_cluser.rb
+++ make_cluser.rb
@@ -1,1 +1,11 @@
 #!/usr/bin/env ruby
+require 'erb'
+require 'optparse'
+
+module_name = ARGV[0]
+clusters = ARGV[1].to_i
+
+for cluster_number in 1..clusters do
+ erb_template = ERB.new(File.read('templates/Cluster.kt'))
+ File.write("#{module_name}/src/main/java/com/airbnb/#{module_name}/Cluster#{cluster_number}.kt", erb_template.result(binding))
+end
---
+++ templates/Cluster.kt
@@ -0,0 +1,90 @@
+package com.airbnb.<%=module_name%>
+
+class Cluster<%=cluster_number%> {
+    var one = "hello"
+    var two = "hello"
+    var three = "hello"
+    var four = "hello"
+    var five = "hello"
+
+    fun one() {
+        java.lang.System.out.println(one);
+    }
+
+    fun two() {
+        java.lang.System.out.println(two);
+    }
+
+    fun three() {
+        java.lang.System.out.println(three);
+    }
+
+    fun four() {
+        java.lang.System.out.println(four);
+    }
+
+    fun five() {
+        java.lang.System.out.println(five);
+    }
+
+}
+
+class ClusterB<%=cluster_number%> {
+
+    var one = "hello"
+    var two = "hello"
+    var three = "hello"
+    var four = "hello"
+    var five = "hello"
+
+    fun one() {
+        java.lang.System.out.println(one);
+    }
+
+    fun two() {
+        java.lang.System.out.println(two);
+    }
+
+    fun three() {
+        java.lang.System.out.println(three);
+    }
+
+    fun four() {
+        java.lang.System.out.println(four);
+    }
+
+    fun five() {
+        java.lang.System.out.println(five);
+    }
+
+}
+
+class ClusterC<%=cluster_number%> {
+
+    var one = "hello"
+    var two = "hello"
+    var three = "hello"
+    var four = "hello"
+    var five = "hello"
+
+    fun one() {
+        java.lang.System.out.println(one);
+    }
+
+    fun two() {
+        java.lang.System.out.println(two);
+    }
+
+    fun three() {
+        java.lang.System.out.println(three);
+    }
+
+    fun four() {
+        java.lang.System.out.println(four);
+    }
+
+    fun five() {
+        java.lang.System.out.println(five);
+    }
+
+}
